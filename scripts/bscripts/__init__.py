1b37b7cbf3cbc631e800691f46f76379c1194e58
willtwr <wrtan.edu@gmail.com>
2019-09-10 10:07:32 +0800

    Update __init__.py

--- scripts/bscripts/__init__.py
+++ scripts/bscripts/__init__.py
@@ -7,12 +7,4 @@ from run_ECO import *
 from run_BACF import *
 from run_SRDCF import *
 from run_GOTURN import *
-from run_SiamFC_nl import *
-#from run_SiamFC_multi_bg_sup import *
-#from run_SiamFC_bg import *
-#from run_SiamFC_nonlinear_upNiter import *
-#from run_SiamFC_scale import *
-#from run_SiamFC_multi_temp import *
-#from run_SiamFC_aug_bna import *
-#from run_SiamFC_multi import *
 
